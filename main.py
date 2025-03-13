340767b1e3750e8d69fb875bc24089a5d63d5585
ansarzeinulla <ansar.zeinulla.a@gmail.com>
2025-03-13 20:20:48 +0500

    J

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import json
 import os
+from datetime import datetime, timedelta
 firebase_key = os.getenv('FIREBASE_KEY')
 if firebase_key:
     firebase_key = json.loads(firebase_key)
@@ -14,7 +15,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("Who is here", callback_data="show_who_is_here")]]
+    keyboard = [[InlineKeyboardButton("Who is here", callback_data="show_who_is_here")],
+    [InlineKeyboardButton("What are the events", callback_data="show_events")]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Hello! I am your bot.", reply_markup=reply_markup)
 
@@ -38,7 +41,27 @@ async def show_who_is_here(update, context):
         for i in range(0, len(result), 4000):
             await update.callback_query.message.reply_text(result[i:i + 4000])
 
+async def show_events(update, context):
+    events_ref = db.collection('events')
+    docs = events_ref.stream()
+    docs_list = list(docs)
+    num_events = len(docs_list)
+    result = ""
+    now = datetime.now()
+    for doc in docs_list:
+        event = doc.to_dict()
+        event_date = datetime.strptime(event['date'], "%Y-%m-%d")  # Assuming date format is "YYYY-MM-DD"
 
+        if now <= event_date <= now + timedelta(days=8):
+            result += f"📅 {event['name']} at {event['location']} on {event['date']}\n"
+        
+    await update.callback_query.answer()
+    if len(result) == 0:
+        await update.callback_query.message.reply_text("No events are scheduled.")
+        return
+    else:
+        for i in range(0, len(result), 4000):
+            await update.callback_query.message.reply_text(result[i:i + 4000])
 
 # Main Application
 app = Application.builder().token(BOT_TOKEN).build()
@@ -46,6 +69,7 @@ app = Application.builder().token(BOT_TOKEN).build()
 # Handlers
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(show_who_is_here))
+app.add_handler(CallbackQueryHandler(show_events))
 
 # Run the Bot
 app.run_polling()
